2c58cccfa46a0b33eaec654fae78b9f47da2704c
anfebit <anfedres.86@gmail.com>
2020-03-01 19:31:32 -0500

    adding topics

--- ros-service-client/ros-service-client.js
+++ ros-service-client/ros-service-client.js
@@ -20,14 +20,12 @@ module.exports = function(RED) {
    
    
     if(config.intype === 'req'){
-      // do the req
-      // advertiseService with (req, res)
       const rosPromise = ros_server(RED, node)
       console.log("promise   ", rosPromise)
       rosPromise.then(nodeHandle =>{
         node.ros = nodeHandle
         console.log('topic ' + config.topicname)
-        console.log('data type   ' + config.typepackage + '.' + config.typename)
+        console.log('data type   ' + config.typepackage + '/' + config.typename)
         node.service = node.ros.advertiseService(config.topicname, config.typepackage + '/' + config.typename, req_res)
         console.log("node.service   ", node.service)
       })
@@ -36,11 +34,6 @@ module.exports = function(RED) {
       })
   }
     else{
-      // do the resp  
-      // node.on('input')
-      // callback using res
-      // assing res 
-      // resolve the promise 
           node.on('input', function(msg) {
           msg.payload = res
           console.log('HERE IS THE Input   ', res)
@@ -51,66 +44,4 @@ module.exports = function(RED) {
     }
     RED.nodes.registerType("ros-service-client", ros_service_client)
 }
-// module.exports = function(RED) {
-//   var ros_service_client = function(config) {
-//     RED.nodes.createNode(this, config)
-//     var node = this
 
-//     node.status({
-//       fill: 'yellow',
-//       shape: 'ring',
-//       text: 'connecting to ros master..'
-//     })
-
-//     node.on('connected to ros', function() {
-//       node.status({
-//         fill: 'green',
-//         shape: 'dot',
-//         text: 'connected'
-//       })
-//     })
-
-//   //  node.on('input', function(msg) {
-//   //     var msgType = rosnodejs.require(config.typepackage)
-//   //     	.msg[config.typename]
-      
-//   //     var x = new msgType()
-//   //     x = msg.payload
-       
-//   //     node.service.publish(x)
-       
-//   //  })
-
-//      var req_rep = (req, resp) => {
-//        console.log('DEBUG:  got req  ' + req)
-//        node.send(resp)
-//        // send response set by the input 
-
-       
-
-//      }
-
-//     node.on('close', function(done) {
-//        debug('Unsubscribing node on topic :', config.topicname)
-//        if (node.ros) {
-//          node.ros.unsubscribe(config.topicname)
-//            .then(function() {
-//              debug('unsubscribed')
-//              done()
-//            })
-//        } else {
-//       done()
-//      }
-//     })
-
-//     ros_server(RED, node)
-//       .then(function(nodeHandle) {
-//         node.ros = nodeHandle
-//         node.service = node.ros.advertiseService(config.topicname, config.typepackage + '/' + config.typename, rep_req)
-//       })
-//       .catch(function(e) {
-//         debug('Er', e)
-//       })
-//   }
-//   RED.nodes.registerType("ros-service-client", ros_service_client)
-// }
